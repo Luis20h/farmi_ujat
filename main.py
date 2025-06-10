788188a03654c980d7d80915b90d756414ac153f
Luis20h <joseded2364@gmail.com>
2025-06-10 01:16:32 -0600

    main

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import flet as ft
-import consulta_airtable as cat
+import interacciones_medicamentosas as inter
+import altas_medicamentos as altas
+import listado_medicamentos as lista
 
-
-def main (page: ft.Page):
+def main(page: ft.Page):
 
     def mostrar_interacciones(e: ft.ControlEvent):
         page.clean()
-        cat.main(page)
+        inter.main(page)
 
-    page.title ="Farma UJAT"
+    def mostrar_altas(e: ft.ControlEvent):
+        page.clean()
+        altas.main(page)
+
+    def mostrar_listado(e: ft.ControlEvent):
+        page.clean()
+        lista.main(page)
+
+    page.title = "Farma UJAT"
     page.theme_mode = "light"
     page.appbar = ft.AppBar(
         title=ft.Text("Farmacia-UJAT", size=48),
-        center_title = True
+        center_title=True
     )
+
     btn_interacciones = ft.FilledButton(
         content=ft.Container(
             content=ft.Column(
                 controls=[
-                    ft.Icon("medication",size=40, color="black"),
-                    ft.Text("Interacciones medicamentosas",text_align=ft.TextAlign.CENTER)
+                    ft.Icon("medication", size=40, color="black"),
+                    ft.Text("Interacciones medicamentosas", text_align=ft.TextAlign.CENTER)
                 ],
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER
             ),
@@ -27,19 +37,20 @@ def main (page: ft.Page):
         ),
         style=ft.ButtonStyle(
             shape=ft.RoundedRectangleBorder(radius=19),
-            side=ft.BorderSide(1,"orange")
+            side=ft.BorderSide(1, "orange")
         ),
         bgcolor="orange100",
         color="black",
         width=200,
         on_click=mostrar_interacciones
     )
+
     btn_altas = ft.FilledButton(
         content=ft.Container(
             content=ft.Column(
                 controls=[
-                    ft.Icon("add_box",size=40, color="black"),
-                    ft.Text("Altas de medicamentos",text_align=ft.TextAlign.CENTER)
+                    ft.Icon("add_box", size=40, color="black"),
+                    ft.Text("Altas de medicamentos", text_align=ft.TextAlign.CENTER)
                 ],
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER
             ),
@@ -47,20 +58,20 @@ def main (page: ft.Page):
         ),
         style=ft.ButtonStyle(
             shape=ft.RoundedRectangleBorder(radius=19),
-            side=ft.BorderSide(1,"ogreen")
+            side=ft.BorderSide(1, "green")
         ),
         bgcolor="green100",
         color="black",
         width=200,
-    
-
+        on_click=mostrar_altas
     )
+
     btn_listar = ft.FilledButton(
         content=ft.Container(
             content=ft.Column(
                 controls=[
-                    ft.Icon("VIEW_LIST",size=40, color="black"),
-                    ft.Text("Listar medicamentos",text_align=ft.TextAlign.CENTER)
+                    ft.Icon("view_list", size=40, color="black"),
+                    ft.Text("Listar medicamentos", text_align=ft.TextAlign.CENTER)
                 ],
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER
             ),
@@ -68,20 +79,22 @@ def main (page: ft.Page):
         ),
         style=ft.ButtonStyle(
             shape=ft.RoundedRectangleBorder(radius=19),
-            side=ft.BorderSide(1,"blue")
+            side=ft.BorderSide(1, "blue")
         ),
         bgcolor="blue100",
         color="black",
-        width=200
+        width=200,
+        on_click=mostrar_listado
     )
+
     row_botones = ft.Row(
         controls=[btn_interacciones, btn_altas, btn_listar],
         alignment=ft.MainAxisAlignment.CENTER,
         spacing=30
     )
-    
+
     page.add(ft.Divider(color="black"))
-    page.add( row_botones)
+    page.add(row_botones)
     page.update()
 
-ft.app(target=main,view=ft.AppView.WEB_BROWSER)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER, route_url_strategy="hash")
